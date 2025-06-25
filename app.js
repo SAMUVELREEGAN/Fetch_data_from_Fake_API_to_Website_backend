b1fe794b59914c9d77540f88644e9bb8de4987d0
Samuvel Reegan <samuelreegan372@gmail.com>
2025-06-25 22:44:35 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const mongoose = require("mongoose");
+
+const app = express();
+
+const MONGODB_URL = "mongodb://127.0.0.1:27017/task_api";
+
+mongoose
+  .connect(MONGODB_URL)
+  .then(() => {
+    console.log(`${MONGODB_URL} is connected`);
+  })
+  .catch((er) => {
+    console.log(er.message);
+  });
+
+// app.get('/' , (req , res) =>{
+//     res.send("Welcome World")
+// })
+
+app.use("/public", express.static("public"));
+
+app.use(require('./routes/ProductRoute'))
+
+app.listen(8000, () => {
+  console.log("Connect");
+});
